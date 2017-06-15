df3b33373b5c651a665514cc57c51fccdb313014
Tom Burns <tom.burns@protonmail.com>
2017-06-14 20:34:59 -0500

    Commented cursor test, added SQL row limiting

--- src/com/burns/Main.java
+++ src/com/burns/Main.java
@@ -15,19 +15,18 @@ public class Main {
         try (
                 Connection conn = DBUtil.getConnection(DBType.MYSQL);
                 Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
+                ResultSet rs = stmt.executeQuery("SELECT tourId, tourName, price FROM tours " + "LIMIT 10, 3")
             ) {
+            Tours.displayData(rs);
 
-            States.displayData(rs);
-
-            rs.last();
+            /*rs.last();
             System.out.println("Number of rows: " + rs.getRow());
             rs.first();
             System.out.println("The first state is: " + rs.getString("stateName"));
             rs.last();
             System.out.println("The last state is: " + rs.getString("stateName"));
             rs.absolute(10);
-            System.out.println("The tenth state is: " + rs.getString("stateName"));
+            System.out.println("The tenth state is: " + rs.getString("stateName"));*/
 
         } catch (SQLException e) {
             DBUtil.processException(e);
