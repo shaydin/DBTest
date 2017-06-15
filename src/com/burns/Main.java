872b85ddf4ff169722f8c58a2b593a1101b489f5
Tom Burns <tom.burns@protonmail.com>
2017-06-14 20:10:28 -0500

    Added helper classes for tables, cursor scrolling test

--- src/com/burns/Main.java
+++ src/com/burns/Main.java
@@ -1,5 +1,8 @@
 package com.burns;
 
+import com.burns.tables.States;
+import com.burns.tables.Tours;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,8 +18,16 @@ public class Main {
                 ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
             ) {
 
+            States.displayData(rs);
+
             rs.last();
             System.out.println("Number of rows: " + rs.getRow());
+            rs.first();
+            System.out.println("The first state is: " + rs.getString("stateName"));
+            rs.last();
+            System.out.println("The last state is: " + rs.getString("stateName"));
+            rs.absolute(10);
+            System.out.println("The tenth state is: " + rs.getString("stateName"));
 
         } catch (SQLException e) {
             DBUtil.processException(e);
